a59157470d3e5a446ae1b12f7c6281ba9c7ebd9d
Daniel Lienert <daniel@lienert.cc>
2020-05-13 08:19:29 +0200

    FEATURE: Add an assetSearchService

---
+++ Neos.Media/Classes/Domain/Repository/AssetSearchService.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Neos\Media\Domain\Repository;
+
+/*
+ * This file is part of the Neos.Media package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+class AssetSearchService implements AssetSearchServiceInterface
+{
+
+    public function findBySearchTermOrTags()
+    {
+        // TODO: Implement findBySearchTermOrTags() method.
+    }
+}
